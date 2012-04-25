2927346f4c513a217ac8ad076e494dd1adbf70e1
Ahmet Emre Aladağ <aladagemre@gmail.com>
2012-04-25 11:47:28 +0300

    Overcomes the import problem caused by version differences in naming django.contrib.syndication.views.feed/Feed

--- notification/views.py
+++ notification/views.py
@@ -4,8 +4,12 @@ from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.syndication.views import feed
 
+try:
+	from django.contrib.syndication.views import feed
+except ImportError:
+	from django.contrib.syndication.views import Feed as feed
+	
 from notification.models import *
 from notification.decorators import basic_auth_required, simple_basic_auth_callback
 from notification.feeds import NoticeUserFeed
